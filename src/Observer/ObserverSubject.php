88a3f7c33c2acf08282926e63b27b5113eeecf07
jijihohococo <jijihohococo@gmail.com>
2021-11-30 02:17:41 +0630

    refactor

--- src/Observer/ObserverSubject.php
+++ src/Observer/ObserverSubject.php
@@ -14,19 +14,17 @@ class ObserverSubject{
 	}
 
 	public function check(string $className){
-		return isset($this->observers[$className]);
+		return isset($this->observers[$className]) && is_array($this->observers[$className]) ;
 	}
 
 	public function use(string $className,string $method,$parameters){
-		if(isset($this->observers[$className]) && is_array($this->observers[$className]) ){
-			foreach($this->observers[$className] as $key => $observer){
-				$observerName=get_class($observer);
-				if(!method_exists($observer, $method)){
-					throw new \Exception("{$observerName} Class doesn't have {$method}", 1);
-				}
-				$reflectionMethod=new ReflectionMethod($observerName,$method);
-				$reflectionMethod->invokeArgs($observer,is_array($parameters) ? $parameters : [$parameters] );
+		foreach($this->observers[$className] as $key => $observer){
+			$observerName=get_class($observer);
+			if(!method_exists($observer, $method)){
+				throw new \Exception("{$observerName} Class doesn't have {$method}", 1);
 			}
+			$reflectionMethod=new ReflectionMethod($observerName,$method);
+			$reflectionMethod->invokeArgs($observer,is_array($parameters) ? $parameters : [$parameters] );
 		}
 	}
 
