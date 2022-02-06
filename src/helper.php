d0e8cf4fbb15085859b911f8cea97c65d5c8a378
jijihohococo <jijihohococo@gmail.com>
2022-02-06 20:06:02 +0630

    add

--- src/helper.php
+++ src/helper.php
@@ -12,7 +12,7 @@ if(!function_exists('jsonResponse')){
 	function jsonResponse(array $data,int $code=200){
 		header('Content-type:application/json');
 		http_response_code($code);
-		echo json_encode($data);
+		return json_encode($data);
 	}
 }
 
