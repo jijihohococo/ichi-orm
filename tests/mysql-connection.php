4eceaf574e610219275131fbf14646c80d6a66a4
jijihohococo <jijihohococo@gmail.com>
2025-12-27 17:32:48 +0900

    add more configurations

--- tests/mysql-connection.php
+++ tests/mysql-connection.php
@@ -7,12 +7,15 @@ use JiJiHoHoCoCo\IchiORM\Database\Connector;
 $connector = new Connector();
 
 $config = [
-    'host'     => '127.0.0.1',
-    'port'     => 3306,
-    'dbname' => 'test',
-    'user_name' => 'root',
+    'driver'        => 'mysql',
+    'host'          => '127.0.0.1',
+    'port'          => 3306,
+    'dbname'        => 'test',
+    'user_name'     => 'root',
     'user_password' => 'root',
-    'charset'  => 'utf8mb4'
+    'charset'       => 'utf8mb4',
+    'collation'     => 'utf8mb4_unicode_ci',
+    'time_zone'     => '+00:00',
 ];
 
 try {
