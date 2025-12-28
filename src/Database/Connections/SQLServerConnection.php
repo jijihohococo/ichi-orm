b4c2298e86f9dcca7b9e2b468a28cd6766bbdc89
jijihohococo <jijihohococo@gmail.com>
2025-12-28 12:55:52 +0900

    fix: update SQL Server encoding option for binary charset

--- src/Database/Connections/SQLServerConnection.php
+++ src/Database/Connections/SQLServerConnection.php
@@ -37,7 +37,7 @@ class SQLServerConnection extends Connection
             if (in_array($charset, ['utf8', 'utf-8'], true)) {
                 $options[PDO::SQLSRV_ATTR_ENCODING] = PDO::SQLSRV_ENCODING_UTF8;
             } elseif ($charset == 'binary') {
-                $options[PDO::SQLSRV_ATTR_ENCODING] = PDO::SQLSRV_ENCODING_BINARY;
+                $options[PDO::SQLSRV_ATTR_ENCODING] = 2;
             } elseif ($charset == 'system') {
                 $options[PDO::SQLSRV_ATTR_ENCODING] = PDO::SQLSRV_ENCODING_SYSTEM;
             } else {
