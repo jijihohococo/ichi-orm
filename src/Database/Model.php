2fe2538e353030343430becf5cc3dbfd3eb29d8f
jijihohococo <jijihohococo@gmail.com>
2026-09-09 00:26:52 +0700

    fix

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -145,7 +145,7 @@ abstract class Model
 
     public static function where(...$parameters)
     {
-        return self::getQueryBuilder()->where($parameters);
+        return self::getQueryBuilder()->where(...$parameters);
     }
 
     public static function from(string $className)
@@ -155,12 +155,12 @@ abstract class Model
 
     public static function whereColumn(...$parameters)
     {
-        return self::getQueryBuilder()->whereColumn($parameters);
+        return self::getQueryBuilder()->whereColumn(...$parameters);
     }
 
     public static function orWhere(...$parameters)
     {
-        return self::getQueryBuilder()->orWhere($parameters);
+        return self::getQueryBuilder()->orWhere(...$parameters);
     }
 
     public static function whereIn(string $field, $value)
@@ -225,17 +225,17 @@ abstract class Model
 
     public static function innerJoin(...$parameters)
     {
-        return self::getQueryBuilder()->innerJoin($parameters);
+        return self::getQueryBuilder()->innerJoin(...$parameters);
     }
 
     public static function leftJoin(...$parameters)
     {
-        return self::getQueryBuilder()->leftJoin($parameters);
+        return self::getQueryBuilder()->leftJoin(...$parameters);
     }
 
     public static function rightJoin(...$parameters)
     {
-        return self::getQueryBuilder()->rightJoin($parameters);
+        return self::getQueryBuilder()->rightJoin(...$parameters);
     }
 
     protected function refersTo(string $class, string $field, string $referField = 'id')
