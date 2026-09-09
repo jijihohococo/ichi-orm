d6275bb5f41872ff08ce3b54711f37558d85391f
jijihohococo <jijihohococo@gmail.com>
2026-09-10 03:25:31 +0700

    fix

--- src/QueryBuilder/QueryBuilder.php
+++ src/QueryBuilder/QueryBuilder.php
@@ -1353,7 +1353,11 @@ class QueryBuilder
                     $i++;
                 }
             } elseif ($current['whereIn'] !== null && !is_array($current['whereIn'])) {
-                $currentField = getCurrentField($this->subQueries, $this->currentField, $this->currentSubQueryNumber);
+                $currentField = preg_replace(
+                    '/__\d+$/',
+                    '', 
+                    getCurrentField($this->subQueries, $this->currentField, $this->currentSubQueryNumber)
+                );
                 $string .= $current['where'] == null && $current['whereColumn'] == null && $current['addTrashed'] == false ? ' WHERE ' . $currentField . ' IN (' . $current['whereIn'] . ')' : ' AND ' . $currentField . ' IN (' . $current['whereIn'] . ')';
             }
         }
