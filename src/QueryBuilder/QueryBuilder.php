0179c46b3a0b912264724e47f206e5682206698f
jijihohococo <jijihohococo@gmail.com>
2026-09-10 01:20:19 +0700

    fix

--- src/QueryBuilder/QueryBuilder.php
+++ src/QueryBuilder/QueryBuilder.php
@@ -1015,10 +1015,11 @@ class QueryBuilder
                 if (is_callable($value) && $this->currentSubQueryNumber == null) {
                     $this->checkUnionQuery();
                     $this->boot();
-                    $this->operators[$field . $where] = makeOperator($operator);
                     $query = $this;
                     $query->setSubQuery($field, $where);
-                    $this->subQueries[$this->currentField . $this->currentSubQueryNumber] = $this->currentSubQueryNumber;
+                    $subQueryKey = $this->currentField . $this->currentSubQueryNumber;
+                    $this->{$where}[$subQueryKey]['operators'][$this->currentField . $where] = makeOperator($operator);
+                    $this->subQueries[$subQueryKey] = $this->currentSubQueryNumber;
                     $value($query);
                     $this->makeDefaultSubQueryData();
                 }
