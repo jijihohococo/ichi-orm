5e9df39dbb4987f215aedd3cb748b4746a6bf136
jijihohococo <jijihohococo@gmail.com>
2026-09-10 22:25:29 +0700

    fix

--- src/QueryBuilder/QueryBuilder.php
+++ src/QueryBuilder/QueryBuilder.php
@@ -2246,6 +2246,9 @@ class QueryBuilder
     public function refersTo(string $class, string $field, string $referField = 'id')
     {
         try {
+            if (!isset($this->{$field})) {
+                return null;
+            }
             checkClass($class);
             if (isset($this->{$field})) {
                 return $class::findBy($referField, $this->{$field});
