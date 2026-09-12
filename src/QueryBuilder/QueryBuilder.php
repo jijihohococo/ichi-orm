ba60eaba3be77096ea77c9fd6f61b246aa6c8da6
jijihohococo <jijihohococo@gmail.com>
2026-09-12 22:45:07 +0700

    fix

--- src/QueryBuilder/QueryBuilder.php
+++ src/QueryBuilder/QueryBuilder.php
@@ -2137,9 +2137,8 @@ class QueryBuilder
             $driver = $pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
 
             $sql = $driver === 'sqlsrv'
-                ? "SELECT * FROM (" . $mainSQL . ") AS paginate_data OFFSET " . $paginate->getStart() . " ROWS FETCH NEXT " . $perPage . " ROWS ONLY"
+                ? "SELECT * FROM (" . $mainSQL . ") AS paginate_data ORDER BY (SELECT NULL) OFFSET " . $paginate->getStart() . " ROWS FETCH NEXT " . $perPage . " ROWS ONLY"
                 : "SELECT * FROM (" . $mainSQL . ") AS paginate_data LIMIT " . $perPage . " OFFSET " . $paginate->getStart();
-            print_r($sql);
             $fields = $this->getFields();
             $stmt = $pdo->prepare($sql);
             bindValues($stmt, $fields);
