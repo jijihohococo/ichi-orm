571fd15a2681b8c439d0251d01a620abc52cd384
jijihohococo <jijihohococo@gmail.com>
2026-09-12 22:37:45 +0700

    test

--- src/QueryBuilder/QueryBuilder.php
+++ src/QueryBuilder/QueryBuilder.php
@@ -2132,8 +2132,12 @@ class QueryBuilder
                 $getOrder .
                 $getGroupBy .
                 $getHaving;
+            
+            $driver = $pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
 
-            $sql = "SELECT * FROM (" . $mainSQL . ") AS paginate_data LIMIT " . $perPage . " OFFSET " . $paginate->getStart();
+            $sql = $driver === 'sqlsrv' 
+                ? "SELECT * FROM (" . $mainSQL . ") AS paginate_data OFFSET " . $paginate->getStart() . " ROWS FETCH NEXT " . $perPage . " ROWS ONLY"
+                : "SELECT * FROM (" . $mainSQL . ") AS paginate_data LIMIT " . $perPage . " OFFSET " . $paginate->getStart();
 
             $fields = $this->getFields();
             $pdo = $this->connectDatabase();
