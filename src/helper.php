83e0c00ac544f050f8d54d16459df47dc4df2f26
jijihohococo <jijihohococo@gmail.com>
2026-09-15 19:41:17 +0700

    fix

--- src/helper.php
+++ src/helper.php
@@ -97,18 +97,37 @@ if (!function_exists('getFirstObject')) {
     }
 }
 
+// if (!function_exists('bindValues')) {
+//     function bindValues($stmt, $fields, &$index = 1)
+//     {
+//         if (is_array($fields)) {
+//             foreach ($fields as $key => $field) {
+//                 if (!is_array($field)) {
+//                     $stmt->bindValue($key + 1, $field, getPDOBindDataType($field));
+//                 }
+//                 if (is_array($field)) {
+//                     return bindValues($stmt, $field);
+//                 }
+//             }
+//         }
+//     }
+// }
 if (!function_exists('bindValues')) {
     function bindValues($stmt, $fields, &$index = 1)
     {
-        if (is_array($fields)) {
-            foreach ($fields as $key => $field) {
-                if (!is_array($field)) {
-                    $stmt->bindValue($key + 1, $field, getPDOBindDataType($field));
-                }
-                if (is_array($field)) {
-                    return bindValues($stmt, $field);
-                }
+        foreach ($fields as $field) {
+            if (is_array($field)) {
+                bindValues($stmt, $field, $index);
+                continue;
             }
+
+            $stmt->bindValue(
+                $index,
+                $field,
+                getPDOBindDataType($field)
+            );
+
+            $index++;
         }
     }
 }
